972e5e99d2c1e418a2dce5ad3236a9620365ce5d
Nolan <111979570+Nolan332933@users.noreply.github.com>
2024-09-02 19:08:00 +0700

    first commit

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,21 @@
+import { Hono, Context } from "hono";
+import { handle } from "hono/vercel";
+import { authHandler, initAuthConfig, verifyAuth } from "@hono/auth-js"
+
+import { getAuthConfig } from "@/auth.config";
+
+import quotes from "./quotes";
+
+export const runtime = "edge";
+
+const app = new Hono().basePath("/api");
+
+app.use("*", initAuthConfig(getAuthConfig));
+app.use("/auth/*", authHandler());
+
+const route = app.route("/quotes", quotes);
+
+export type AppType = typeof route;
+
+export const GET = handle(app);
+export const POST = handle(app);
